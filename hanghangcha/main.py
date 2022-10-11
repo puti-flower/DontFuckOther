d938ee4bf27e8e3dd911ef776a274f44dee204f8
puti <ptx@live.com>
2022-10-11 16:45:33 +0800

    行行查解密

---
+++ hanghangcha/main.py
@@ -0,0 +1,20 @@
+from email.mime import base
+import json
+import requests
+import base64
+import re
+from Crypto.Cipher import AES
+
+
+def aes_decrypt(b64_text: str, key: str):
+    key = key.encode()
+    cipher = AES.new(key, mode=AES.MODE_ECB)
+    text = base64.b64decode(b64_text)
+    raw_text = cipher.decrypt(text).decode()
+    text = re.sub("[\x00-\x09|\x0b-\x0c|\x0e-\x1f]", "", raw_text)
+    return json.loads(text)
+
+
+url = "https://api.hanghangcha.com/hhc/tag"
+r = requests.get(url)
+print(aes_decrypt(r.json()["data"], "3sd&d24h@$udD2s*"))
